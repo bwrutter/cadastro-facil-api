8c8d03c312af2eac2c505103307c992979fbc4b9
Bryan W Rutter <86786094+ibtech-bryan@users.noreply.github.com>
2023-01-21 15:34:17 -0300

    Initial commit

---
+++ src/main/java/com/api/cadastrofacil/CadastroFacilApplication.java
@@ -0,0 +1,13 @@
+package com.api.cadastrofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cadastrofacil/CadastroFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cadastrofacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
